d062690799e0395d5119c0b67d35238fd6d0b22f
Tejas Kumbhar <145969163+TejasK30@users.noreply.github.com>
2023-12-15 14:45:11 +0530

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,11 +4,14 @@ import { AiOutlineMenu } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleMenu = () => {
-    setIsOpen(!isOpen)
+    setIsOpen(!isOpen);
+  }
+
+  const closeMenu = () => {
+    setIsOpen(false);
   }
 
   return (
@@ -17,13 +20,13 @@ const Navbar = () => {
         <h2>Tejas Kumbhar</h2>
         <AiOutlineMenu className='menu-bar' onClick={handleMenu} />
         <ul className={`menu ${isOpen ? 'open' : ''}`}>
-          <li><Link to='/' className="link">Home</Link></li>
-          <li><Link to='/about' className="link">About</Link></li>
-          <li><Link to='/contact' className="link">Contact</Link></li>
+          <li><Link to='/' className="link" onClick={closeMenu}>Home</Link></li>
+          <li><Link to='/about' className="link" onClick={closeMenu}>About</Link></li>
+          <li><Link to='/contact' className="link" onClick={closeMenu}>Contact</Link></li>
         </ul>
       </nav>
     </>
   )
 }
 
-export default Navbar
+export default Navbar;

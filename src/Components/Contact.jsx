e4bae62e5694de499a4e217600f7142d3ce1d47e
TejasK30 <tejas.k6389@gmail.com>
2024-07-15 20:14:47 +0530

    bug fixes

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -6,7 +6,6 @@ const service_id = import.meta.env.VITE_SERVICE_ID
 const template_id = import.meta.env.VITE_TEMPLATE_ID
 const public_key = import.meta.env.VITE_PUBLIC_KEY
 
-console.log(service_id, template_id, public_key);
 
 const ContactMe = () => {
   const form = useRef()

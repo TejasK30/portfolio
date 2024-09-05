143a8151fa6cb7a6f2b6b5b27b1b1d9f9e0555ee
TejasK30 <tejas.k6389@gmail.com>
2024-09-06 00:25:25 +0530

    edit: skills

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,3 +1,4 @@
+import { FaDocker } from "react-icons/fa";
 import { FaCss3, FaHtml5, FaJs, FaNodeJs, FaReact } from 'react-icons/fa'
 import { SiExpress } from 'react-icons/si'
 import { BiLogoTailwindCss, BiLogoMongodb } from 'react-icons/bi'
@@ -16,6 +17,7 @@ const Skills = () => {
         <BiLogoTailwindCss style={{color: "blue", fontSize: "40px", margin: '10px'}} />
         <FaReact className='animate-spin' style={{color: "blue", fontSize: "40px", margin: '10px'}} />
         <BiLogoMongodb style={{color: "green", fontSize: "40px", margin: '10px'}} />
+        <FaDocker style={{color: "blue", fontSize: "40px", margin: '10px'}}/>
       </div>
     </div>
   )
